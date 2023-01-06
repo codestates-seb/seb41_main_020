b2fa56e3b109b737e2c8a1b4939e1af08f9d81e2
Sungho95 <tjdgh1785@gmail.com>
2023-01-06 16:59:59 +0900

    initial commit

--- server/indiego/src/main/java/codestates/frogroup/indiego/IndieGoApplication.java
+++ server/indiego/src/main/java/codestates/frogroup/indiego/IndieGoApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class IndiegoApplication {
+public class IndieGoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(IndiegoApplication.class, args);
+		SpringApplication.run(IndieGoApplication.class, args);
 	}
 
 }
