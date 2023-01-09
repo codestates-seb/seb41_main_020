dd5db974e48b1a561a0071ee87d110e8b5e50841
zemma0618 <zemma0618@gmail.com>
2023-01-09 18:59:44 +0900

    #11 TicketDetail 페이지 ContentHeader 레이아웃 작성, mixin 수정
    mixin misc 컬러 추가, 폰트 크기 조정

--- client/src/Pages/Tickets/TicketsDetail.jsx
+++ client/src/Pages/Tickets/TicketsDetail.jsx
@@ -1,8 +1,9 @@
 // 최상단에는 리액트 컴포넌트
+import Header from "../../Components/Header.jsx";
+
 // 그다음에는 로컬 모듈
 import styled from "styled-components/macro";
 import breakpoint from "../../styles/breakpoint";
-import { Mixin } from "react";
 import {
   primary,
   secondary,
@@ -15,31 +16,100 @@ import {
 import React from "react";
 
 const Container = styled.div`
-  display: flex;
-  width: 100%;
-  height: max-content;
-  margin: 0 auto;
-  justify-content: center;
   background-color: aliceblue;
+  display: flex;
+  height: max-content;
+  justify-content: center;
+  margin: 0 auto;
+  width: 100%;
 `;
 
 const ContentContainer = styled.div`
   display: flex;
   flex-direction: column;
   width: 90%;
-  background-color: beige;
 `;
 
 const ContentHeaderContainer = styled.div`
+  align-items: flex-start;
   display: flex;
+  height: max-content;
+  min-height: 150px;
+  padding: 20px 47px;
   width: 100%;
-  padding: 0 47px;
+`;
+
+const HeaderTitleContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+
+  > h1 {
+    all: unset;
+    color: ${primary.primary500};
+    font-size: ${dtFontSize.xxlarge};
+    font-weight: 700;
+  }
+
+  > h2 {
+    all: unset;
+    color: ${sub.sub400};
+    font-size: ${dtFontSize.medium};
+    font-weight: 400;
+    margin: 5px 0 0 0;
+  }
+`;
+
+const HeaderButtonContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 205px;
+  height: max-content;
+  margin: 0 0 0 20px;
+`;
+
+const PillButton = styled.button`
+  all: unset;
+  color: white;
+  cursor: pointer;
+  width: max-content;
+  height: max-content;
+  padding: 5px 20px;
+  border-radius: 20px;
+  font-weight: 600;
+  font-size: ${dtFontSize.medium};
+  background-color: ${(props) => props.color};
+
+  &:hover {
+    background-color: ${(props) => props.hoverColor};
+  }
 `;
 
 export default function TicketsDetail() {
   return (
-    <Container>
-      <ContentContainer>dfsfd</ContentContainer>
-    </Container>
+    <>
+      <Header />
+      <Container>
+        <ContentContainer>
+          <ContentHeaderContainer>
+            <HeaderTitleContainer>
+              <h1>공연 상세페이지</h1>
+              <h2>서울 / 종로구 / Rock Night Party</h2>
+            </HeaderTitleContainer>
+            <HeaderButtonContainer>
+              <PillButton
+                color={primary.primary300}
+                hoverColor={secondary.secondary500}
+              >
+                수정하기
+              </PillButton>
+              <PillButton color={misc.red} hoverColor={misc.lightred}>
+                삭제하기
+              </PillButton>
+            </HeaderButtonContainer>
+          </ContentHeaderContainer>
+        </ContentContainer>
+      </Container>
+    </>
   );
 }
