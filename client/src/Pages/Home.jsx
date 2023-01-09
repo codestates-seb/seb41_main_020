fc732bd5ba28b2fe630327f8672728ffea96b5e1
Yeon <yeonhwan619@gmail.com>
2023-01-09 17:07:39 +0900

    #8 배너 컴포넌트 추가

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -1,11 +1,22 @@
 import React from "react";
+
 import Header from "../Components/Header.jsx";
+import Banner from "../Components/Main/Banner.jsx";
+
+import styled from "styled-components";
+
+const MainContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100vw;
+  height: 100vh;
+`;
 
 export default function Home() {
   return (
-    <div>
+    <MainContainer>
       <Header />
-      <div>Home</div>
-    </div>
+      <Banner />
+    </MainContainer>
   );
 }
