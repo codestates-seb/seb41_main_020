462647a963ced905b649434c17b0f2b7f4baf3b4
zemma0618 <zemma0618@gmail.com>
2023-01-09 16:57:46 +0900

    #11 git 충돌 해결
    develop -> feat 브랜치 pull 과정에서 난 git 충돌을 해결함

--- client/src/Pages/Tickets/TicketsDetail.jsx
+++ client/src/Pages/Tickets/TicketsDetail.jsx
@@ -1,5 +1,45 @@
+// 최상단에는 리액트 컴포넌트
+// 그다음에는 로컬 모듈
+import styled from "styled-components/macro";
+import breakpoint from "../../styles/breakpoint";
+import { Mixin } from "react";
+import {
+  primary,
+  secondary,
+  sub,
+  misc,
+  dtFontSize,
+  mbFontSize,
+} from "../../styles/mixins";
+// 그다음에는 라이브러리
 import React from "react";
 
+const Container = styled.div`
+  display: flex;
+  width: 100%;
+  height: max-content;
+  margin: 0 auto;
+  justify-content: center;
+  background-color: aliceblue;
+`;
+
+const ContentContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 90%;
+  background-color: beige;
+`;
+
+const ContentHeaderContainer = styled.div`
+  display: flex;
+  width: 100%;
+  padding: 0 47px;
+`;
+
 export default function TicketsDetail() {
-  return <div>TicketsDetail</div>;
+  return (
+    <Container>
+      <ContentContainer>dfsfd</ContentContainer>
+    </Container>
+  );
 }
