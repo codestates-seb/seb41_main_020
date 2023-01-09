5591d6d967a3b4bb3481f28490be81327bb1a124
kimjeonghui <20190896@sungshin.ac.kr>
2023-01-09 17:28:03 +0900

    #4 공연 도메인 dto 작성 ShowCommentDto, ShowDto, ShowReservationDto

---
+++ server/src/main/java/codestates/frogroup/indiego/domain/show/dto/ShowCommentDto.java
@@ -0,0 +1,41 @@
+package codestates.frogroup.indiego.domain.show.dto;
+
+import codestates.frogroup.indiego.domain.member.entity.Member;
+import codestates.frogroup.indiego.domain.show.entity.Show;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import javax.persistence.*;
+
+public class ShowCommentDto {
+
+    @Getter
+    @AllArgsConstructor
+    public static class Post{
+        private Long id;
+        private ShowDto.Post show;
+        private Integer score;
+        private String comment;
+    }
+
+    @Getter
+    @AllArgsConstructor
+    @Builder
+    public static class Patch{
+        private Long id;
+        private ShowDto.Patch show;
+        private Integer score;
+        private String comment;
+    }
+
+    @Getter
+    @AllArgsConstructor
+    @Builder
+    public static class Response{
+        private Long id;
+        private ShowDto.Response show;
+        private Integer score;
+        private String comment;
+    }
+}
