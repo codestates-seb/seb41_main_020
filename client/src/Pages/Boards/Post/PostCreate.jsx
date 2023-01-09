572560c2aa5da854826f198300cad110ffb014c2
Yeon <yeonhwan619@gmail.com>
2023-01-09 13:37:33 +0900

    #6 클라이언트 라우팅
    리액트 라우터를 통해 클라이언트 라우팅

---
+++ client/src/Pages/Boards/Post/PostCreate.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function PostCreate() {
+  return <div>PostCreate</div>;
+}
---
+++ client/src/Pages/Boards/Post/PostEdit.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function PostEdit() {
+  return <div>PostEdit</div>;
+}
