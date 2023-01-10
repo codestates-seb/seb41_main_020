a2a42eaa6d52e0fda8637379ec956d90da827f2b
Sungho95 <tjdgh1785@gmail.com>
2023-01-10 11:28:43 +0900

    #27 - 게시글 초기 세팅
    - ArticleMapper
    - ArticleService
    - ArticleServiceImpl

--- server/src/main/java/codestates/frogroup/indiego/domain/article/mapper/ArticleMapper.java
+++ server/src/main/java/codestates/frogroup/indiego/domain/article/mapper/ArticleMapper.java
@@ -1,4 +1,37 @@
 package codestates.frogroup.indiego.domain.article.mapper;
 
+import codestates.frogroup.indiego.domain.article.entity.Article;
+import codestates.frogroup.indiego.domain.article.entity.dto.ArticleDto;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
 public interface ArticleMapper {
+
+    @Mapping(source = "title", target = "board.title")
+    @Mapping(source = "content", target = "board.content")
+    @Mapping(source = "image", target = "board.image")
+    @Mapping(source = "category", target = "board.category")
+    Article articlePostToArticle(ArticleDto.Post requestBody);
+
+    @Mapping(source = "title", target = "board.title")
+    @Mapping(source = "content", target = "board.content")
+    @Mapping(source = "image", target = "board.image")
+    @Mapping(source = "category", target = "board.category")
+    Article articlePatchToArticle(ArticleDto.Patch requestBody);
+
+    @Mapping(source = "board.title", target = "title")
+    @Mapping(source = "board.content", target = "content")
+    @Mapping(source = "board.image", target = "image")
+    @Mapping(source = "board.category", target = "category")
+    ArticleDto.Response articleToArticleResponse(Article article);
+
+    @Mapping(source = "board.title", target = "title")
+    @Mapping(source = "board.content", target = "content")
+    @Mapping(source = "board.image", target = "image")
+    @Mapping(source = "board.category", target = "category")
+    List<ArticleDto.Response> articlesToArticleResponses(List<Article> articles);
+
 }
