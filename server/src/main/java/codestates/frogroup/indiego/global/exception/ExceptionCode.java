aaf67ebecdaa3c85bc36cc46205131ce02c3c42b
Sungho95 <tjdgh1785@gmail.com>
2023-01-11 17:52:07 +0900

    #27 - 서비스 로직에 필요한 ErrorCode 추가
    - 회원 권한 없음 예외 추가
    - 게시글을 찾을 수 없는 경우 예외 추가
    - 게시글 댓글을 찾을 수 없는 경우 예외 추가

--- server/src/main/java/codestates/frogroup/indiego/global/exception/ExceptionCode.java
+++ server/src/main/java/codestates/frogroup/indiego/global/exception/ExceptionCode.java
@@ -6,7 +6,9 @@ public enum ExceptionCode {
     // 필요한 예외처리 추가
     MEMBER_NOT_FOUND(404, "Member not found"),
     MEMBER_EXISTS(409, "Member exists"),
-    ARTICLE_NOT_FOUND(404, "게시글을 찾을 수 없습니다.");
+    MEMBER_NO_PERMISSION(406, "권한이 없습니다."),
+    ARTICLE_NOT_FOUND(404, "게시글을 찾을 수 없습니다."),
+    ARTICLE_COMMENT_NOT_FOUND(404, "해당 게시글의 댓글을 찾을 수 없습니다.");
 
     @Getter
     private int status;
