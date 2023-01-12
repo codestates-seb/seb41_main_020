b3fd19b51775c3ba155157feb5d55cac6cca9c48
Sungho95 <tjdgh1785@gmail.com>
2023-01-12 14:27:43 +0900

    #44 - 게시글 댓글 인증 토큰 적용
    - 회원 인증을 적용한 컨트롤러 작성

--- server/src/main/java/codestates/frogroup/indiego/domain/article/controller/ArticleCommentController.java
+++ server/src/main/java/codestates/frogroup/indiego/domain/article/controller/ArticleCommentController.java
@@ -5,6 +5,7 @@ import codestates.frogroup.indiego.domain.article.service.ArticleCommentService;
 import codestates.frogroup.indiego.domain.member.entity.Member;
 import codestates.frogroup.indiego.domain.member.repository.MemberRepository;
 import codestates.frogroup.indiego.global.dto.SingleResponseDto;
+import codestates.frogroup.indiego.global.security.auth.loginresolver.LoginMemberId;
 import codestates.frogroup.indiego.global.stub.StubData;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -29,14 +30,12 @@ public class ArticleCommentController {
      */
     @PostMapping
     public ResponseEntity postArticleComment(@PathVariable("article-id") Long articleId,
+                                             @LoginMemberId Long memberId,
                                              @Valid @RequestBody ArticleCommentDto.Post articleCommentPostDto) {
 
-        // TODO: 임시용 추후 삭제
-        Member member = new StubData().member;
-        memberRepository.save(member);
 
         ArticleCommentDto.Response response =
-                articleCommentService.createArticleComment(articleId, member.getId(), articleCommentPostDto);
+                articleCommentService.createArticleComment(articleId, memberId, articleCommentPostDto);
 
         return new ResponseEntity<>(new SingleResponseDto<>(response), HttpStatus.CREATED);
     }
@@ -47,14 +46,12 @@ public class ArticleCommentController {
     @PatchMapping("/{comment-id}")
     public ResponseEntity patchArticleComment(@PathVariable("article-id") Long articleId,
                                               @PathVariable("comment-id") Long commentId,
+                                              @LoginMemberId Long memberId,
                                               @Valid @RequestBody ArticleCommentDto.Patch articleCommentPatchDto) {
 
-        // TODO: 임시용 추후 삭제
-        Member member = new StubData().member;
-        memberRepository.save(member);
 
         ArticleCommentDto.Response response =
-                articleCommentService.updateArticleComment(articleId, commentId, member.getId(), articleCommentPatchDto);
+                articleCommentService.updateArticleComment(articleId, commentId, memberId, articleCommentPatchDto);
 
         return new ResponseEntity<>(new SingleResponseDto<>(response), HttpStatus.OK);
     }
@@ -64,13 +61,10 @@ public class ArticleCommentController {
      */
     @DeleteMapping("/{comment-id}")
     public ResponseEntity deleteArticleComment(@PathVariable("article-id") Long articleId,
-                                               @PathVariable("comment-id") Long commentId) {
+                                               @PathVariable("comment-id") Long commentId,
+                                               @LoginMemberId Long memberId) {
 
-        // TODO: 임시용 추후 삭제
-        Member member = new StubData().member;
-        memberRepository.save(member);
-
-        articleCommentService.deleteArticleComment(articleId, commentId, member.getId());
+        articleCommentService.deleteArticleComment(articleId, commentId, memberId);
 
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
@@ -80,13 +74,11 @@ public class ArticleCommentController {
      */
     @PutMapping("/{comment-id}")
     public ResponseEntity articleCommentLike(@PathVariable("article-id") Long articleId,
-                                             @PathVariable("comment-id") Long commentId) {
+                                             @PathVariable("comment-id") Long commentId,
+                                             @LoginMemberId Long memberId) {
 
-        // TODO: 임시용 추후 삭제
-        Member member = new StubData().member;
-        memberRepository.save(member);
 
-        articleCommentService.articleCommentLike(articleId, commentId, member.getId());
+        articleCommentService.articleCommentLike(articleId, commentId, memberId);
 
         return new ResponseEntity<>(HttpStatus.OK);
     }
