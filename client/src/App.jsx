fc9a4819f3882cca73fe511825a3a1217b06ce89
zemma0618 <zemma0618@gmail.com>
2023-01-13 10:38:10 +0900

    #11 배너 부분 div 사이즈 설정 변경
    width를 100%, height를 width의 비율에 맞춰서 줄어들도록 설정

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -27,7 +27,7 @@ import { Route, Routes } from "react-router-dom";
 function App() {
   return (
     <>
-      <Header />
+      {/* <Header /> */}
       <Routes>
         <Route path="/" element={<Home />}></Route>
 
