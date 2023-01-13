2cd1d8d5c3534bd682caf5f3b1545aeabbdecc49
kimjeonghui <20190896@sungshin.ac.kr>
2023-01-13 16:36:35 +0900

    #47 - 공연 예약 구현 mapper

---
+++ server/src/main/java/codestates/frogroup/indiego/domain/show/mapper/ShowMapper.java
@@ -0,0 +1,66 @@
+package codestates.frogroup.indiego.domain.show.mapper;
+
+import codestates.frogroup.indiego.domain.show.dto.ShowDto;
+import codestates.frogroup.indiego.domain.show.entity.Show;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.ReportingPolicy;
+
+@Mapper(componentModel = "spring" , unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface ShowMapper {
+
+    @Mapping(source = "title", target = "showBoard.board.title")
+    @Mapping(source = "content", target = "showBoard.board.content")
+    @Mapping(source = "image", target = "showBoard.board.image")
+    @Mapping(source = "category", target = "showBoard.board.category")
+    @Mapping(source = "price", target = "showBoard.price")
+    @Mapping(source = "address", target = "showBoard.address")
+    @Mapping(source = "expiredAt", target = "showBoard.expiredAt")
+    @Mapping(source = "showAt", target = "showBoard.showAt")
+    @Mapping(source = "latitude", target = "coordinate.latitude")
+    @Mapping(source = "longitude", target = "coordinate.longitude")
+    @Mapping(source = "detailImage", target = "showBoard.detailImage")
+    Show showPostDtoToShow(ShowDto.Post post);
+
+    @Mapping(source = "title", target = "showBoard.board.title")
+    @Mapping(source = "content", target = "showBoard.board.content")
+    @Mapping(source = "image", target = "showBoard.board.image")
+    @Mapping(source = "category", target = "showBoard.board.category")
+    @Mapping(source = "price", target = "showBoard.price")
+    @Mapping(source = "address", target = "showBoard.address")
+    @Mapping(source = "expiredAt", target = "showBoard.expiredAt")
+    @Mapping(source = "showAt", target = "showBoard.showAt")
+    @Mapping(source = "longitude", target = "coordinate.longitude")
+    @Mapping(source = "latitude", target = "coordinate.latitude")
+    @Mapping(source = "detailImage", target = "showBoard.detailImage")
+    Show showPatchDtoToShow(ShowDto.Patch patch);
+
+    @Mapping(source = "showBoard.board.title", target = "title")
+    @Mapping(source = "showBoard.board.content", target = "content")
+    @Mapping(source = "showBoard.board.image", target = "image")
+    @Mapping(source = "showBoard.board.category", target = "category")
+    @Mapping(source = "showBoard.price", target = "price")
+    @Mapping(source = "showBoard.address", target = "address")
+    @Mapping(source = "showBoard.expiredAt", target = "expiredAt")
+    @Mapping(source = "showBoard.showAt", target = "showAt")
+    @Mapping(source = "coordinate.latitude", target = "latitude")
+    @Mapping(source = "coordinate.longitude", target = "longitude")
+    @Mapping(source = "status", target = "status")
+    @Mapping(source = "showBoard.detailImage", target = "detailImage")
+    ShowDto.postResponse showToShowPostResponse(Show show);
+
+    @Mapping(source = "showBoard.board.title", target = "title")
+    @Mapping(source = "showBoard.board.content", target = "content")
+    @Mapping(source = "showBoard.board.image", target = "image")
+    @Mapping(source = "showBoard.board.category", target = "category")
+    @Mapping(source = "showBoard.price", target = "price")
+    @Mapping(source = "showBoard.address", target = "address")
+    @Mapping(source = "showBoard.expiredAt", target = "expiredAt")
+    @Mapping(source = "showBoard.showAt", target = "showAt")
+    @Mapping(source = "id", target = "id")
+    @Mapping(source = "coordinate.latitude", target = "latitude")
+    @Mapping(source = "coordinate.longitude", target = "longitude")
+    @Mapping(source = "status", target = "status")
+    @Mapping(source = "showBoard.detailImage", target = "detailImage")
+    ShowDto.Response showToShowResponse(Show show);
+}
