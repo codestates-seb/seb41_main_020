357057ee5688bced60f5053b107aa3956d7c040d
codemodel6@gmail.com <codemodel6@gmail.com>
2023-01-13 17:18:03 +0900

    #10 - searchbar 구현 완 및 분리 완 및 answerDummy 오타 수정
    searchBar 구현 및 컴포넌트 분리하여 component/Board 폴더 내에 작성하였습니다
    answerDummy 의 like cunt -> like count 로 오타 수정하였습니다

---
+++ client/src/Components/Board/SearchBar.jsx
@@ -0,0 +1,57 @@
+import styled from "styled-components";
+import React from "react";
+import { primary, sub } from "../../styles/mixins";
+import search from "../../assets/search.svg";
+
+const SearchBarDiv = styled.div`
+  display: flex;
+  text-align: center;
+  justify-content: center;
+
+  .aSearchBarDiv {
+    width: 30vw;
+    border: 3px solid ${sub.sub500};
+    position: relative;
+    display: flex;
+    align-items: center;
+    border-radius: 20px;
+    padding-right: 4px;
+
+    .searchBarInput {
+      width: 100%;
+      border-radius: 20px;
+      padding: 10px;
+      height: 40px;
+      border: none;
+
+      &:focus-within {
+        outline: none;
+      }
+    }
+
+    .searchImage {
+      width: 17px;
+      height: 17px;
+    }
+    &:focus-within {
+      border: 3px solid ${primary.primary200};
+    }
+  }
+
+  .listButton {
+    background-color: blue;
+  }
+`;
+
+const SearchBar = ({ placeholder }) => {
+  return (
+    <SearchBarDiv>
+      <div className="aSearchBarDiv">
+        <input className="searchBarInput" placeholder={placeholder} />
+        <img className="searchImage" src={search} alt="돋보기"></img>
+      </div>
+    </SearchBarDiv>
+  );
+};
+
+export default SearchBar;
