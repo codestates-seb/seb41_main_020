fa721af90d8b897984fb397c23958ab8284c4a0f
Yeon <yeonhwan619@gmail.com>
2023-01-13 19:53:38 +0900

    머지 오류 관련'

--- client/src/Pages/Tickets/TicketsDetail.jsx
+++ client/src/Pages/Tickets/TicketsDetail.jsx
@@ -384,7 +384,7 @@ export default function TicketsDetail() {
                   Ut enim ad minim veniam, quis nostrud exercitation ullamco
                   laboris nisi ut aliquip ex ea commodo consequat. Duis aute
                   irure dolor in reprehenderit in voluptate velit esse cillum
-                  dolore eu fugiat nulla pariatur.
+                  dolore eu fugiat nulla pariatur.!!!!
                 </span>
               </div>
               <div>
