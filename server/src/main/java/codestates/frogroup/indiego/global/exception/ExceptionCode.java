0a239bc2ed07fa8f9a4b6a2d269d5b1d3baa4fd9
kimjeonghui <20190896@sungshin.ac.kr>
2023-01-15 20:49:37 +0900

    #76 - 공연 북마크 구현

--- server/src/main/java/codestates/frogroup/indiego/global/exception/ExceptionCode.java
+++ server/src/main/java/codestates/frogroup/indiego/global/exception/ExceptionCode.java
@@ -27,7 +27,8 @@ public enum ExceptionCode {
     ARTICLE_NOT_FOUND(404, "게시글을 찾을 수 없습니다."),
     ARTICLE_COMMENT_NOT_FOUND(404, "해당 게시글의 댓글을 찾을 수 없습니다."),
 
-    SHOW_NOT_FOUND(404, "Show not found");
+    SHOW_NOT_FOUND(404, "Show not found"),
+    BOOKMARK_NOT_FOUND(404, "");
 
     @Getter
     private int status;
