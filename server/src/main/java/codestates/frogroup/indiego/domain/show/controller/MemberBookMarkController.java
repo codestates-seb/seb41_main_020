de0cad96b8448c8b1cb24286fc1d562d77d058d3
kimjeonghui <20190896@sungshin.ac.kr>
2023-01-16 10:49:58 +0900

    #76 북마크 리팩토링

--- server/src/main/java/codestates/frogroup/indiego/domain/show/controller/MemberBookMarkController.java
+++ server/src/main/java/codestates/frogroup/indiego/domain/show/controller/MemberBookMarkController.java
@@ -2,6 +2,7 @@ package codestates.frogroup.indiego.domain.show.controller;
 
 import codestates.frogroup.indiego.domain.member.entity.MemberBookMark;
 import codestates.frogroup.indiego.domain.show.service.MemberBookMarkService;
+import codestates.frogroup.indiego.global.security.auth.loginresolver.LoginMemberId;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,8 +18,9 @@ import java.util.Optional;
 public class MemberBookMarkController {
     private final MemberBookMarkService bookMarkService;
     @PutMapping("/{show-id}")
-    public ResponseEntity putBookMark(@PathVariable("show-id") long showId){
-        HttpStatus httpStatus = bookMarkService.manageBookMark(showId);
+    public ResponseEntity putBookMark(@PathVariable("show-id") long showId,
+                                      @LoginMemberId long memnerId){
+        HttpStatus httpStatus = bookMarkService.manageBookMark(showId, memnerId);
         return new ResponseEntity<>(
                 httpStatus
         );
