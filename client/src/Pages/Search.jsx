518ba955e6b7b77621fba47916771bce918835c0
Yeon <yeonhwan619@gmail.com>
2023-01-16 23:06:30 +0900

    #92 카카오맵 api 클라이언트 적용중'

--- client/src/Pages/Search.jsx
+++ client/src/Pages/Search.jsx
@@ -1,4 +1,12 @@
-import React, { useState, useRef, useEffect } from "react";
+import React, { useState, useEffect } from "react";
+
+import SearchBar from "../Components/Main/SearchBar.jsx";
+
+import { primary, dtFontSize, sub } from "../styles/mixins";
+import breakpoint from "../styles/breakpoint.js";
+import marker from "../assets/marker.svg";
+
+import { locationDummyArr } from "../DummyData/locationDummy";
 
 import styled from "styled-components";
 
@@ -16,55 +24,149 @@ const MapContainer = styled.div`
   height: max-content;
   flex-direction: column;
   align-items: center;
+  margin-top: 60px;
+`;
+
+const ContentHeaderContainer = styled.div`
+  align-items: flex-start;
+  display: flex;
+  flex-direction: column;
+  height: max-content;
+  align-items: space-between;
+  min-height: 100px;
+  padding: 30px 47px;
+  width: 90%;
+
+  h1 {
+    color: ${primary.primary500};
+  }
+
+  h2 {
+    all: unset;
+    color: ${sub.sub400};
+    font-size: ${dtFontSize.medium};
+    font-weight: 400;
+    margin: 5px 0 0 0;
+  }
+
+  @media screen and (max-width: ${breakpoint.mobile}) {
+    min-height: 100px;
+    padding: 20px 5.13%;
+    width: 100%;
+  }
 `;
 
 export default function Search() {
-  const mapContainer = useRef(null);
   const { kakao } = window;
   const [locationPoint, setLocationPoint] = useState({});
 
+  const markerImageSrc = marker;
+  const markerImageSize = new kakao.maps.Size(64, 64);
+  const markerImageOption = { offset: new kakao.maps.Point(27, 69) };
+
+  const markerImage = new kakao.maps.MarkerImage(
+    markerImageSrc,
+    markerImageSize,
+    markerImageOption
+  );
+
+  // 지도 초기 생성
   useEffect(() => {
-    var container = document.getElementById("map"); //지도를 담을 영역의 DOM 레퍼런스
-    var options = {
-      //지도를 생성할 때 필요한 기본 옵션
-      center: new kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
-      level: 3, //지도의 레벨(확대, 축소 정도)
-    };
+    const mapContainer = document.getElementById("map"), // 지도를 표시할 div
+      mapOption = {
+        center: new kakao.maps.LatLng(37.556, 126.9723), // 지도의 중심좌표
+        level: 3, // 지도의 확대 레벨
+      };
 
-    const mapTypeControl = new kakao.maps.MapTypeControl();
+    // 지도를 표시할 div와  지도 옵션으로  지도를 생성합니다
+    const map = new kakao.maps.Map(mapContainer, mapOption);
+
+    // 클러스터 관련
+    const clusterer = new kakao.maps.MarkerClusterer({
+      map: map,
+      averageCenter: true,
+      minLevel: 3,
+    });
+
+    // 마커 동적 생성
+    const markers = locationDummyArr.map((locObj) => {
+      // 마커 생성
+
+      const marker = new kakao.maps.Marker({
+        position: new kakao.maps.LatLng(locObj.latitude, locObj.longitude),
+        clickable: true,
+        image: markerImage,
+      });
+
+      // 지도에 마커 추가
+      marker.setMap(map);
+
+      // 마커 팝업 마크업
+      const markerClickPopup = `<div style="padding:5px;">${locObj.title}</div>`;
+
+      const markerHoverPopup = `<div style="padding:5px; background-color:blue; color:white;">${locObj.title}</div>`;
+
+      // 카카오 맵 이밴트 핸들러
+      const popupWindow = new kakao.maps.InfoWindow({
+        content: markerClickPopup,
+        removable: true,
+      });
+
+      const hoverWindow = new kakao.maps.InfoWindow({
+        content: markerHoverPopup,
+      });
+
+      // 카카오 맵 이벤트 리스너
+      kakao.maps.event.addListener(
+        marker,
+        "click",
+        (() => {
+          return function () {
+            popupWindow.open(map, marker);
+            const moveLocation = new kakao.maps.LatLng(
+              locObj.latitude,
+              locObj.longitude
+            );
+            map.setLevel(4, { anchor: moveLocation });
+          };
+        })(locObj)
+      );
+
+      kakao.maps.event.addListener(marker, "mouseover", function () {
+        hoverWindow.open(map, marker);
+      });
+
+      kakao.maps.event.addListener(marker, "mouseout", function () {
+        hoverWindow.close();
+      });
+
+      return marker;
+    });
+
+    // 클러스터에 배열 형태로 된 마커들을 전달하면 클러스터 생성
+    clusterer.addMarkers(markers);
+
+    // 일반 지도와 스카이뷰로 지도 타입을 전환할 수 있는 지도타입 컨트롤을 생성합니다
+    var mapTypeControl = new kakao.maps.MapTypeControl();
+
+    // 지도에 컨트롤을 추가해야 지도위에 표시됩니다
+    // kakao.maps.ControlPosition은 컨트롤이 표시될 위치를 정의하는데 TOPRIGHT는 오른쪽 위를 의미합니다
+    map.addControl(mapTypeControl, kakao.maps.ControlPosition.TOPRIGHT);
+
+    // 지도 확대 축소를 제어할 수 있는  줌 컨트롤을 생성합니다
     var zoomControl = new kakao.maps.ZoomControl();
-
-    var map = new kakao.maps.Map(container, options);
-    map.addControl(mapTypeControl, kakao.maps.ControlPosition.TOPRIGHT); //지도 생성 및 객체 리턴
     map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);
-
-    var marker = new kakao.maps.Marker({
-      // 지도 중심좌표에 마커를 생성합니다
-      position: map.getCenter(),
-    });
-    // 지도에 마커를 표시합니다
-    marker.setMap(map);
-
-    // 지도에 클릭 이벤트를 등록합니다
-    // 지도를 클릭하면 마지막 파라미터로 넘어온 함수를 호출합니다
-    kakao.maps.event.addListener(map, "click", function (mouseEvent) {
-      // 클릭한 위도, 경도 정보를 가져옵니다
-      var latlng = mouseEvent.latLng;
-
-      // 마커 위치를 클릭한 위치로 옮깁니다
-      marker.setPosition(latlng);
-    });
   });
 
   return (
     <Container>
+      <ContentHeaderContainer>
+        <h1>공연 찾기</h1>
+        <h2>찾고자 하는 공연을 위치기반으로 검색합니다.</h2>
+      </ContentHeaderContainer>
+      <SearchBar />
       <MapContainer>
-        <h1>Map</h1>
-        <div
-          ref={mapContainer}
-          id="map"
-          style={{ width: "100%", height: "400px" }}
-        ></div>
+        <div id="map" style={{ width: "100%", height: "400px" }}></div>
       </MapContainer>
     </Container>
   );
