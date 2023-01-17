b466ce9bc788d6170e2d84ba137644c2e0c98620
Lee JaeHyeok <dhfif718@gmail.com>
2023-01-17 20:03:52 +0900

    #87 - 파일 업로드 추상화 완료
    1. 게시글, 공연 서비스로직에서 사용가능하도록 경로 추가

--- server/src/main/java/codestates/frogroup/indiego/global/fileupload/ImageUploadService.java
+++ server/src/main/java/codestates/frogroup/indiego/global/fileupload/ImageUploadService.java
@@ -4,6 +4,6 @@ import org.springframework.web.multipart.MultipartFile;
 
 public interface ImageUploadService {
 
-	String StoreImage(MultipartFile img);
+	String StoreImage(MultipartFile img, AwsS3Path awsS3Path);
 
 }
