88a70b7153699891e18d4ac400873c0aff38315a
kimjeonghui <20190896@sungshin.ac.kr>
2023-01-17 21:51:21 +0900

    Changes by 박성호

--- server/src/main/java/codestates/frogroup/indiego/domain/show/service/ShowService.java
+++ server/src/main/java/codestates/frogroup/indiego/domain/show/service/ShowService.java
@@ -13,6 +13,7 @@ import codestates.frogroup.indiego.global.exception.ExceptionCode;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
