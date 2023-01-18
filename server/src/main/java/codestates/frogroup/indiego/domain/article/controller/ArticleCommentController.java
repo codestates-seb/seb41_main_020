aac85e6c3ca8332183213a25fd66716e791b5217
Sungho95 <80394894+Sungho95@users.noreply.github.com>
2023-01-18 21:27:43 +0900

    @CrossOrigin 추가

--- server/src/main/java/codestates/frogroup/indiego/domain/article/controller/ArticleCommentController.java
+++ server/src/main/java/codestates/frogroup/indiego/domain/article/controller/ArticleCommentController.java
@@ -10,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @Slf4j
+@CrossOrigin
 @RestController
 @RequestMapping("/articles/{article-id}/comments")
 @RequiredArgsConstructor
