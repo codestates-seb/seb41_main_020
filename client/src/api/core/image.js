700df05ee7e6ae9c2b7255da26b62e809192ce8d
codemodel6@gmail.com <codemodel6@gmail.com>
2023-01-25 17:57:41 +0900

    #240 - 이미지 base64 를 img 태그로 변경 진행 초기

---
+++ client/src/api/core/image.js
@@ -0,0 +1,75 @@
+/* eslint-disable prettier/prettier */
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import useIsLoginStore from "../../store/useIsLoginStore";
+
+const instance = axios.create({
+  baseURL: process.env.REACT_APP_SERVER_URI,
+  headers: {
+    Accept: "*/*",
+    "Content-Type": "multipart/form-data",
+  },
+  withCredentials: true,
+});
+
+instance.interceptors.request.use(
+  async (config) => {
+    if (localStorage.getItem("accessToken")) {
+      config.headers["Authorization"] = `Bearer ${localStorage.getItem(
+        "accessToken"
+      )}`;
+      console.log(localStorage.getItem("accessToken"));
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+instance.interceptors.response.use(
+  function (response) {
+    return response;
+  },
+  async function (error) {
+    const { config, response } = error;
+
+    if (response.status === 400) {
+      const originalRequest = config;
+
+      const headers = {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+        Refresh: localStorage.getItem("refreshToken"),
+      };
+
+      try {
+        const data = await axios({
+          url: `${process.env.REACT_APP_SERVER_URI}/members/reissue`,
+          method: "GET",
+          headers,
+        });
+        if (data) {
+          localStorage.setItem(
+            "accessToken",
+            data.headers.get("Authorization").split(" ")[1]
+          );
+          originalRequest.headers.Authorization = `Bearer ${
+            data.headers.get("Authorization").split(" ")[1]
+          }`;
+          return await axios.request(originalRequest);
+        }
+      } catch (error) {
+        axios({
+          url: `${process.env.REACT_APP_SERVER_URI}/members/logout`,
+          method: "GET",
+          headers,
+        });
+      }
+      return Promise.reject(error);
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default instance;
