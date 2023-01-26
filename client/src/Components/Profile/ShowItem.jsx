e24f9f57ab51f3d7df48227a85aa31d982d0f351
zemma0618 <zemma0618@gmail.com>
2023-01-26 23:42:09 +0900

    #257 (유저 프로필 페이지) 프로필 이미지 업로드 기능 추가, 내가 예매한 공연 관리 기능 추가

--- client/src/Components/Profile/ShowItem.jsx
+++ client/src/Components/Profile/ShowItem.jsx
@@ -13,10 +13,14 @@ import {
   dtFontSize,
   mbFontSize,
 } from "../../styles/mixins";
+import instance from "../../api/core/default.js";
+import useIsLoginStore from "../../store/useIsLoginStore.js";
 
 //라이브러리 및 라이브러리 메소드
 import React from "react";
 import styled from "styled-components/macro";
+import { useNavigate } from "react-router-dom";
+import { useMutation } from "@tanstack/react-query";
 
 const ShowItemContainer = styled.li`
   all: unset;
@@ -47,8 +51,7 @@ const ItemContentContainer = styled.div`
   > div {
     display: flex;
 
-    > .poster-image-area {
-      background-color: ${primary.primary300};
+    > .poster-image {
       height: 100px;
       width: 75px;
 
@@ -144,19 +147,67 @@ const ItemContentContainer = styled.div`
 `;
 
 export default function ShowItem({ reservationData }) {
+  const { isLogin, setIsLogin } = useIsLoginStore((state) => state);
+  const navigate = useNavigate();
+
+  const deleteReservation = () => {
+    return instance({
+      method: "delete",
+      url: `/shows/reservations/${reservationData.id}`,
+    });
+  };
+
+  const deleteReservationOnsuccess = () => {
+    window.alert("예매 취소가 완료되었습니다.");
+  };
+
+  const deleteReservationOnError = (error) => {
+    if (
+      error.response.status === 400 &&
+      error.response.data.message === "Token Expired"
+    ) {
+      window.alert("다시 로그인해주세요.");
+      localStorage.clear();
+      setIsLogin(false);
+      navigate("/");
+    } else if (error.response.status === 500) {
+      window.alert("일시적인 오류입니다. 잠시 후에 다시 시도해주세요.");
+    }
+  };
+
+  const { mutate: deleteReservationData } = useMutation({
+    mutationFn: deleteReservation,
+    onSuccess: deleteReservationOnsuccess,
+    onError: deleteReservationOnError,
+  });
+
+  const handledeleteReservation = () => {
+    deleteReservationData();
+  };
+
   return (
     <ShowItemContainer>
       <ItemContentContainer>
         <div>
-          <div className="poster-image-area" />
+          <img
+            alt="poster"
+            className="poster-image"
+            src={reservationData && reservationData.image}
+          />
           <div className="show-info-container">
             <div className="title-and-provider-container">
-              <span>{reservationData && reservationData.title}</span>
-              <span>{reservationData && reservationData.nickname}</span>
+              <span>
+                {reservationData && reservationData.title} /{" "}
+                {reservationData && reservationData.nickname}
+              </span>
+              <span>
+                {" "}
+                티켓: {reservationData && reservationData.ticketCount}매
+              </span>
             </div>
             <div className="period-and-location-container">
               <span className="period">
-                예약일시: {reservationData && reservationData.date}
+                공연일시: {reservationData && reservationData.date}
               </span>
               <span className="location">서울특별시 종로구 종로운동장</span>
             </div>
@@ -166,7 +217,11 @@ export default function ShowItem({ reservationData }) {
           <button className="map-button">
             <img alt="kakao map icon" src={KakaoMapIcon} />
           </button>
-          <PillButton color={misc.red} hoverColor={misc.lightred}>
+          <PillButton
+            color={misc.red}
+            hoverColor={misc.lightred}
+            onClick={handledeleteReservation}
+          >
             취소
           </PillButton>
         </div>
