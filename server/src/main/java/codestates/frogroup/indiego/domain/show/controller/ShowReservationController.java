8ea7f02298d3f9e7710009a630a506ef6f2889ac
kimjeonghui <20190896@sungshin.ac.kr>
2023-01-27 11:06:19 +0900

    #235 유저 공연 예약 조회 반환값 수정

--- server/src/main/java/codestates/frogroup/indiego/domain/show/controller/ShowReservationController.java
+++ server/src/main/java/codestates/frogroup/indiego/domain/show/controller/ShowReservationController.java
@@ -73,13 +73,18 @@ public class ShowReservationController {
     }
 
 
+    //USER 예약 조회
     @GetMapping
     public ResponseEntity getShows(@LoginMemberId Long memberId){
         List<ShowReservation> showReservationList =showReservationRepository.findByMember_Id(memberId);
         List<ShowReservationDto.Response> responses = mapper.showsReservationsToShowResvationResponses(showReservationList);
+        //ToDo mapper에서 처리하도록
         for(int i=0; i< responses.size(); i++){
-            responses.get(i).setAddress(showReservationList.get(i).getShow().getShowBoard().getDetailAddress());
+            responses.get(i).setDetailAddress(showReservationList.get(i).getShow().getShowBoard().getDetailAddress());
             responses.get(i).setImage(showReservationList.get(i).getShow().getShowBoard().getBoard().getImage());
+            responses.get(i).setAddress(showReservationList.get(i).getShow().getShowBoard().getAddress());
+            responses.get(i).setLongitude(showReservationList.get(i).getShow().getCoordinate().getLongitude());
+            responses.get(i).setLatitude(showReservationList.get(i).getShow().getCoordinate().getLatitude());
         }
         return new ResponseEntity(
                 new SingleResponseDto<>(setExpireds(showReservationList, responses)),
